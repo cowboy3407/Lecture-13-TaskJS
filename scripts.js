619f6e954ce510a00f262da014b91d6f92cda30f
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2024-10-30 18:42:06 +0400

    js

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let name1 = "Gigi"
+let lastname = "Nakhutsrishvili"
+let age = 17
+let mail = "nakhutsrishviligigi@gmail.com"
+let phone = 597882227
+let adult = false
+
+console.log("My name is " + name1 + " Last name is " + lastname + ", I am %c" + age + "%c, Contact mail " + mail + ", phone number %c" + phone, 
+    "color: red;", 
+    "", 
+    "color: yellow;" )
+
+
+
+let name2 = "James"
+let lastname1 = "Bond"
+let age1 = 40
+let mail1 = "jamesbond@gmail.com"
+let phone1 = 951981263
+let adult1 = true
+
+console.log ("My name is" + name2 + " Last name is " + lastname1 + " ,I am %c" + age1 + "%c,Contact mail " + mail1 + " ,phone number %c" + phone1, 
+    "color:red;",
+     "", 
+     "color:yellow;",
+    );
+
